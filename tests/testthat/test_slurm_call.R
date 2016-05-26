3f3ccde7816907dd76b265d782ef59d381c6e7ee
Philippe Marchand <pmarchand@rstudio01.research.sesync.org>
2016-05-26 10:52:44 -0400

    Skip tests on CRAN

--- tests/testthat/test_slurm_call.R
+++ tests/testthat/test_slurm_call.R
@@ -1,25 +1,28 @@
 library(rslurm)
 context("slurm_call")
 
-# Test slurm_call locally
-
-msg <- capture.output(
-    sjob <- slurm_call(function(x, y) x * 2 + y, list(x = 5, y = 6), 
-                       jobname = "test^\\* call", submit = FALSE)
-)
-
-test_that("slurm_job name is correctly edited", {
-    expect_equal(sjob$jobname, "test_call")
-})
-
-setwd(paste0("_rslurm_", sjob$jobname))
-tryCatch(system("Rscript --vanilla slurm_run.R"), finally = setwd(".."))
-res <- get_slurm_out(sjob)
-
-test_that("slurm_call returns correct output", {
-    expect_equal(res, 16)
-})
-
-# Pause to make sure temporary folder is free to be deleted
-Sys.sleep(1)
-cleanup_files(sjob)
+if (Sys.getenv("NOT_CRAN") == "true") {
+    
+    # Test slurm_call locally
+    
+    msg <- capture.output(
+        sjob <- slurm_call(function(x, y) x * 2 + y, list(x = 5, y = 6), 
+                           jobname = "test^\\* call", submit = FALSE)
+    )
+    
+    test_that("slurm_job name is correctly edited", {
+        expect_equal(sjob$jobname, "test_call")
+    })
+    
+    setwd(paste0("_rslurm_", sjob$jobname))
+    tryCatch(system("Rscript --vanilla slurm_run.R"), finally = setwd(".."))
+    res <- get_slurm_out(sjob)
+    
+    test_that("slurm_call returns correct output", {
+        expect_equal(res, 16)
+    })
+    
+    # Pause to make sure temporary folder is free to be deleted
+    Sys.sleep(1)
+    cleanup_files(sjob)
+}
