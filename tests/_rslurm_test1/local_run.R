0c46a7fc047923aa3def1ca53aa326cef1df9d28
Ian Carroll <icarroll@sesync.org>
2017-08-17 11:39:39 -0400

    remove _rslurm files from tests

--- tests/_rslurm_test1/local_run.R
+++
@@ -1,4 +0,0 @@
-for (i in 1:2 - 1) {
-Sys.setenv(SLURM_ARRAY_TASK_ID = i)
-source('slurm_run.R')
-}
--- tests/_rslurm_test1/submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --array=0-1
-#SBATCH --job-name=test1
-#SBATCH --output=slurm_%a.out
-/uufs/chpc.utah.edu/sys/installdir/R/3.2.3i_rh7/lib64/R/bin/Rscript --vanilla slurm_run.R
