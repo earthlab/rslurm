4ac8c3487e70161a9199d050983492f297354633
Ian Carroll <icarroll@sesync.org>
2017-10-18 17:40:10 -0400

    update and rebuild vignettes results

--- vignettes/_rslurm_test_apply/slurm_run.R
+++ vignettes/_rslurm_test_apply/slurm_run.R
@@ -5,15 +5,17 @@ library(utils, quietly = TRUE)
 library(grDevices, quietly = TRUE)
 library(graphics, quietly = TRUE)
 library(stats, quietly = TRUE)
-library(devtools, quietly = TRUE)
+library(testthat, quietly = TRUE)
 library(rslurm, quietly = TRUE)
 .rslurm_func <- readRDS('f.RDS')
 .rslurm_params <- readRDS('params.RDS')
 .rslurm_id <- as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
 .rslurm_istart <- .rslurm_id * 5 + 1
 .rslurm_iend <- min((.rslurm_id + 1) * 5, nrow(.rslurm_params))
-.rslurm_result <- do.call(parallel::mcMap, c(.rslurm_func,
+.rslurm_result <- do.call(parallel::mcmapply, c(
+    FUN = .rslurm_func,
     .rslurm_params[.rslurm_istart:.rslurm_iend, , drop = FALSE],
-    mc.cores = 2))
+    mc.cores = 2,
+    SIMPLIFY = FALSE))
 
 saveRDS(.rslurm_result, file = paste0('results_', .rslurm_id, '.RDS'))
