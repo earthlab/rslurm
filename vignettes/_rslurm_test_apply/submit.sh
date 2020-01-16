e1d02a255edb1b927e730268435bd4309392f93c
Quentin Read <quentin.read@gmail.com>
2020-01-16 14:58:27 -0500

    Rebuild pkgdown site with all documentation updated

--- vignettes/_rslurm_test_apply/submit.sh
+++ vignettes/_rslurm_test_apply/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --array=0-1
+#SBATCH --cpus-per-task=2
 #SBATCH --job-name=test_apply
 #SBATCH --output=slurm_%a.out
-/usr/lib/R/bin/Rscript --vanilla slurm_run.R
+C:/PROGRA~1/R/R-36~1.0/bin/x64/Rscript --vanilla slurm_run.R
